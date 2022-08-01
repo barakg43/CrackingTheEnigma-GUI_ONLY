c0ac393c39a7cc9b9079be5ae7d7773669483b01
Nikol Sapozhnikov <nikolsa@mta.ac.il>
2022-08-01 11:22:49 +0300

    case2+case3

---
+++ src/menuEngine/MachineDataDTO.java
@@ -0,0 +1,34 @@
+package menuEngine;
+
+public class MachineDataDTO {
+
+    private final int NumberOfReflectors;
+    private final int NumberOfRotorsInUse;
+    private final int[] RotorsId;
+    private final int[] NotchNums;
+
+
+
+    public MachineDataDTO(int numberOfReflectors,int numOfRotorsInUse,int[] rotorsIdArray,int[] notchArray) {
+        NumberOfReflectors = numberOfReflectors;
+        NumberOfRotorsInUse=numOfRotorsInUse;
+        RotorsId=rotorsIdArray;
+        NotchNums=notchArray;
+    }
+
+    public int getNumberOfReflectors() {
+        return NumberOfReflectors;
+    }
+
+    public int[] getNotchNums() {
+        return NotchNums;
+    }
+
+    public int[] getRotorsId() {
+        return RotorsId;
+    }
+
+    public int getNumberOfRotorsInUse() {
+        return NumberOfRotorsInUse;
+    }
+}
